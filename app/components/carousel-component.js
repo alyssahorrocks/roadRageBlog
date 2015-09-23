7e2333b7429d2db9c1c0d70f8ff226714d2ae1a5
Alyssa Horrocks & Lisa Duschek <student@epicodus.com>
2015-09-23 16:43:16 -0700

    add funtioning carousel

--- app/components/carousel-component.js
+++ app/components/carousel-component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  currentArticle: Ember.computed('articles', function() {
+    return this.get('articles').get('firstObject');
+  }),
+  actions: {
+    nextArticle() {
+      var lastArticle = this.get('articles').pop;
+      this.get('articles').shiftObject(lastArticle);
+    }
+  }
 });
